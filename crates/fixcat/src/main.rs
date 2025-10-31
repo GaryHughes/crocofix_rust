9487364ab7a6df8646bca3c419dec00bb6c2b1a9
Gary Hughes <garyedwardhughes@gmail.com>
2025-10-31 14:10:46 +1100

    If the order book does not process a message don't print the reason. This is expected for messages the order book does not care about.

--- crates/fixcat/src/main.rs
+++ crates/fixcat/src/main.rs
@@ -103,10 +103,10 @@ fn decode_and_print_line(line: &String, options: &Options, order_book: &mut Orde
                     Ok(()) => {
                         let _ = match order_report.print(&mut stdout(), order_book) {
                             Ok(bytes) => bytes,
-                            Err(error) => eprintln!("{:?}", error)
+                            Err(error) => (eprintln!("{:?}", error))
                         };
                     },
-                    Err(error) => eprintln!("{:?}", error)
+                    Err(_) => ()
                 }
             }
         }
